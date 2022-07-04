2a0a41b2aebb8897aef909be3051974c1f70d35a
alsgud2659 <alsgud2659@naver.com>
2022-07-04 20:11:01 +0900

    출력메소드

---
+++ workspace/day01/src/day01/PrintTest.java
@@ -0,0 +1,29 @@
+package day01;
+
+public class PrintTest {
+	public static void main(String[] args) {
+		
+		// 주석
+		// 1. 소스코드에 설명글을 달 때
+		// 2. 지금 당장 사용하지 않는 코드를 번역하고 싶지 않을 때
+		
+		// 주석의 종류
+		/*
+		 * 1. 한줄주석 : Ctrl + /
+		 * 2. 범위주석 : Ctrl + Shift + /, Ctrl + Shift + \
+		 * 
+		 */
+		
+		//이름을 출력하는 부분
+//		System.out.print("한동석");
+		
+		//나이를 출력하는 부분
+		System.out.print("20살");
+	}
+}
+
+
+
+
+
+
---
+++ workspace/day01/src/day01/PrintTest2.java
@@ -0,0 +1,22 @@
+package day01;
+
+public class PrintTest2 {
+	public static void main(String[] args) {
+		
+		//제어 문자
+		//반드시 따옴표 안에서 작성한다.
+		
+		/*
+		 * \n	: new line, 줄바꿈, 개행문자
+		 * \t	: tab, 위 아래 줄 간격 맞춰 띄기
+		 * \"	: " 표현
+		 * \'	: ' 표현
+		 * \\	: \ 표현
+		 */
+		
+//		System.out.print("\"한동석\"\n");
+		System.out.println("한동석");
+		System.out.print("20살");
+		
+	}
+}
